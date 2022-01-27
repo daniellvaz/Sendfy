f530f1a129782e71cc529aa0045647bbea3eaf0d
daniellvaz <daniellmvaz@gmail.com>
2022-01-27 00:13:51 -0300

    :sparkles: window view
    create window view function

--- electron/src/tray/trayMenu.js
+++ electron/src/tray/trayMenu.js
@@ -2,7 +2,7 @@ const { Menu, app, ipcMain } = require("electron");
 const systemSchema = require("../store/system.schema");
 
 const { alwaysOnTop } = systemSchema.get("system");
-
+const path = app.getPath("userData");
 const menuTemplate = [
   {
     label: "Sempre ao topo",
@@ -13,7 +13,9 @@ const menuTemplate = [
     label: "Abrir",
     click: () => ipcMain.emit("open-window"),
   },
-  { label: "Sair", click: () => app.quit() },
+  { label: "Sair", click: app.quit },
+  { type: "separator" },
+  { label: "Configurações", click: () => systemSchema.openInEditor },
 ];
 
 const menu = new Menu.buildFromTemplate(menuTemplate);
